84306c392991f31851070d724a053d83afe49813
Jeff Spacek <Jeffrey.spacek@gmail.com>
2024-03-04 09:02:03 -0800

    update citysearch.test.js and citysearch.js

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,18 +1,31 @@
 import { useState } from "react";
 
-const CitySearch = () => {
+const CitySearch = ({ allLocations }) => {
   const [showSuggestions, setShowSuggestions] = useState(false);
+  const [query, setQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
   return (
     <div id="city-search">
       <input
         type="text"
         className="city"
         placeholder="Search for a city"
+        value={query}
         onFocus={() => setShowSuggestions(true)}
+        onChange={handleInputChanged}
       />
-      {showSuggestions ? <ul className="suggestions"></ul>: null}
+      {showSuggestions ? (
+        <ul className="suggestions">
+          {suggestions.map((suggestion) => {
+            return <li key={suggestion}>{suggestion}</li>;
+          })}
+          <li key="See all cities">
+            <b>See all cities</b>
+          </li>
+        </ul>
+      ) : null}
     </div>
-  )
- }
-   
-   export default CitySearch;
+  );
+};
+
+export default CitySearch;
