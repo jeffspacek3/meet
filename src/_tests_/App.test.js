16e2daed295a5a077ff5cc4cb28b4fb5eb96e2dd
Jeff Spacek <Jeffrey.spacek@gmail.com>
2024-03-03 21:10:06 -0800

    fix missing syntax in app.test.js and eventlist.test.js

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -2,14 +2,19 @@
 import { render } from "@testing-library/react";
 import App from "../App";
 
+describe('<App /> component', () => {
+  let AppDOM;
+  beforeEach(() => {
+    AppDOM = render(<App />).container.firstChild;
+  })
 
 test("renders list of events", () => {
-  const AppDom = render(<App />).containerfirstChild;
-  return expect(AppDom.QuerySelector("#event-list")).toBeInTheDocument();
+  const AppDom = render(<App />).container.firstChild;
+  return expect(AppDom.querySelector("#event-list")).toBeInTheDocument();
 });
 
 test("render CitySearch", () => {
-  const AppDom = render().allowContainerFirstChild;
-  return expect(AppDom.QuerySelector("#city-search")).toBeInTheDocument();
+  const AppDom = render(<App/>).allowContainerFirstChild;
+  return expect(AppDom.querySelector("#city-search")).toBeInTheDocument();
 });
 
--- src/_tests_/EventList.test.js
+++ src/_tests_/EventList.test.js
@@ -1,17 +1,22 @@
-import { render } from '@testing-library/react';
-import EventList from '../components/EventList';
+import { render } from "@testing-library/react";
+import EventList from "../components/EventList";
 
-describe('<EventList /> component', () => {
-  test('has an element with "list" role', () => {
-    const view = render(<EventList />);
-    expect(view.queryBy("list")).toBeInTheDocument();
+describe("<EventList /> component", () => {
+  let EventListComponent;
+  beforeEach(() => {
+    EventListComponent = render(<EventList />);
   });
 
-  test('renders correct number of events', () => {
-    const EventListComponent = render(<EventList events=
-      {[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]
-    } />);
-    expect(EventListComponent.screen.getAllByRole("listitem")).toHaveLength(4);
+  test('has an element with "list" role', () => {
+    const view = render(<EventList />);
+    expect(EventListComponent.getByRole("list")).toBeInTheDocument();
+  });
+
+  test("renders correct number of events", async () => {
+    EventListComponent.rerender = (
+      <EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />
+    );
+    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
   });
 });
 
