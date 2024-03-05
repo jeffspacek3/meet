362603756bdf9204e176ad1ebca8e5f340591245
Jeff Spacek <Jeffrey.spacek@gmail.com>
2024-03-04 22:55:35 -0800

    update syntax on citysearch.js, evenlist.js, citysearch.test.js, and eventlisttest.js

--- src/_tests_/EventList.test.js
+++ src/_tests_/EventList.test.js
@@ -1,5 +1,6 @@
 import { render } from "@testing-library/react";
 import EventList from "../components/EventList";
+import { getEvents } from '../api';
 
 describe("<EventList /> component", () => {
   let EventListComponent;
@@ -13,10 +14,9 @@ describe("<EventList /> component", () => {
   });
 
   test("renders correct number of events", async () => {
-    EventListComponent.rerender = (
-      <EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />
-    );
-    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
+    const allEvents = await getEvents();
+    EventListComponent.rerender(<EventList events={allEvents} />);
+    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
   });
 });
 
--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -1,15 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-const CitySearch = ({ allLocations }) => {
+const CitySearch = ({ allLocations, setCurrentCity, setInfoAlert }) => {
   const [showSuggestions, setShowSuggestions] = useState(false);
   const [query, setQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+
+  const handleInputChanged = (event) => {
+    const value = event.target.value;
+    const filteredLocations = allLocations
+      ? allLocations.filter((location) => {
+          return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+        })
+      : [];
+
+    setQuery(value);
+    setSuggestions(filteredLocations);
+
+    let infoText;
+    if (filteredLocations.length === 0) {
+      infoText =
+        "We can not find the city you are looking for. Please try another city";
+    } else {
+      infoText = "";
+    }
+    setInfoAlert(infoText);
+  };
+
+  const handleItemClicked = (event) => {
+    const value = event.target.textContent;
+    setQuery(value);
+    setShowSuggestions(false); // to hide the list
+    setCurrentCity(value);
+    setInfoAlert("");
+  };
+
+  useEffect(() => {
+    setSuggestions(allLocations);
+  }, [`${allLocations}`]);
+
   return (
     <div id="city-search">
       <input
         type="text"
         className="city"
-        placeholder="Search for a city"
+        placeholder="Search for city"
         value={query}
         onFocus={() => setShowSuggestions(true)}
         onChange={handleInputChanged}
@@ -17,9 +51,13 @@ const CitySearch = ({ allLocations }) => {
       {showSuggestions ? (
         <ul className="suggestions">
           {suggestions.map((suggestion) => {
-            return <li key={suggestion}>{suggestion}</li>;
+            return (
+              <li onClick={handleItemClicked} key={suggestion}>
+                {suggestion}
+              </li>
+            );
           })}
-          <li key="See all cities">
+          <li key="See all cities" onClick={handleItemClicked}>
             <b>See all cities</b>
           </li>
         </ul>
