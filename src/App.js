98cdd130f6e42fee1af9c001014784300a28061e
Jeff Spacek <Jeffrey.spacek@gmail.com>
2024-03-14 23:06:21 -0700

    fix trouble shooting in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import CitySearch from "./components/CitySearch";
 import EventList from "./components/EventList";
 import NumberOfEvents from "./components/NumberOfEvents";
+import { InfoAlert, ErrorAlert } from "./components/Alert";
 import { useEffect, useState } from "react";
 import { extractLocations, getEvents } from "./api";
 import "./App.css";
-import { InfoAlert } from "./components/Alert";
+import { InfoAlert, ErrorAlert } from "./components/Alert";
 
 const App = () => {
   const [allLocations, setAllLocations] = useState([]);
@@ -12,6 +13,7 @@ const App = () => {
   const [events, setEvents] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
   const [infoAlert, setInfoAlert] = useState("");
+  const [errorAlert, setErrorAlert] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -31,13 +33,18 @@ const App = () => {
     <div className="App">
       <div className="alerts-container">
         {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
+        {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
       </div>
+
       <CitySearch
         allLocations={allLocations}
         setCurrentCity={setCurrentCity}
         setInfoAlert={setInfoAlert}
       />
-      <NumberOfEvents setCurrentNOE={setCurrentNOE} />
+      <NumberOfEvents
+        setCurrentNOE={setCurrentNOE}
+        SetErrorAlert={setErrorAlert}
+      />
       <EventList events={events} />
     </div>
   );
