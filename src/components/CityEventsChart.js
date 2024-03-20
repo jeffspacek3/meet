1339f0e7e8677bc523bc957d9cad0f2b36c4b914
Jeff Spacek <Jeffrey.spacek@gmail.com>
2024-03-19 22:25:34 -0700

    fix rename file to "CityEventsChart.js"

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -39,7 +39,12 @@ const CityEventsChart = ({ allLocations, events }) => {
       >
         <CartesianGrid />
         <XAxis type="category" dataKey="city" name="City" />
-        <YAxis type="number" dataKey="count" name="Number of events" allowDecimals={false} />
+        <YAxis
+          type="number"
+          dataKey="count"
+          name="Number of events"
+          allowDecimals={false}
+        />
         <Tooltip cursor={{ strokeDasharray: "3 3" }} />
         <Scatter name="A school" data={data} fill="#8884d8" />
       </ScatterChart>
